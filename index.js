938d1f0c16d0975726775c727760ac6636deb0a3
mkarsten06 <146241171+mkarsten06@users.noreply.github.com>
2023-11-30 18:36:41 +0000

    caesar-cipher2

---
+++ index.js
@@ -0,0 +1,10 @@
+const plainText = "Cryptography is typically bypassed not penetration"
+console.log({plainText})
+
+function caesarCipher(str, shift) {
+
+}
+
+var shiftFactor = 30
+const cipherText = caesarCipher(plainText, shiftFactor)
+console.log({cipherText})
